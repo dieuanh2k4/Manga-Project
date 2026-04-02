354eeb644b9216fde9b2085f824d668a25d723c5
edgozd <zeusedom@gmail.com>
2026-04-02 20:24:19 +0700

    feat: initialize app framework and layout

---
+++ webfluttermanga/lib/main.dart
@@ -0,0 +1,33 @@
+﻿import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const MangaApp());
+}
+
+class MangaApp extends StatelessWidget {
+  const MangaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MangaProject',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.transparent, 
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF141414),
+          elevation: 0,
+        ),
+        colorScheme: const ColorScheme.dark(
+          primary: Color(0xFFE50914),
+          surface: Color(0xFF2B2B2C),
+          onSurface: Colors.white,
+        ),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
