64364ed578e0074a2b44228ee33f8ec72d6d042d
Văn Đức Anh <ducanhku@gmail.com>
2026-05-25 21:57:26 +0700

    chore: update API URL to localhost in constants

--- web_admin/lib/core/constants/constants.dart
+++ web_admin/lib/core/constants/constants.dart
@@ -1,1 +1,1 @@
-const String newAPIBaseURL = String.fromEnvironment('API_URL', defaultValue: 'http://192.168.8.159:5219/api/');
+const String newAPIBaseURL = String.fromEnvironment('API_URL', defaultValue: 'http://localhost:5219/api/');
