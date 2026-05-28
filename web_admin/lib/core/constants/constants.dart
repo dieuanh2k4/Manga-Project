cf9c27c165f55a5cf1127794474df3a86d813221
Nguyễn Diệu Anh <121100269+dieuanh2k4@users.noreply.github.com>
2026-05-28 09:50:37 +0700

    fix docker compose

--- web_admin/lib/core/constants/constants.dart
+++ web_admin/lib/core/constants/constants.dart
@@ -2,6 +2,6 @@
 // const String newAPIBaseURL = 'http://10.76.200.178:5001/api/';
 const String newAPIBaseURL = String.fromEnvironment(
   'API_BASE_URL',
-  defaultValue: 'http://192.168.0.195:5001/api/',
-  // defaultValue: 'http://10.76.200.178:5001/api/',
+  // defaultValue: 'http://192.168.0.195:5001/api/',
+  defaultValue: 'http://10.76.200.178:5001/api/',
 );
